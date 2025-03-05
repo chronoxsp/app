628f517b5b7dc7cdbfc4f4820a3f2f4b80af6da1
dsainz <dsainz@tragsa.es>
2025-03-05 13:04:57 +0100

    ServletInitializer

--- src/main/java/com/ejemplo/App.java
+++ src/main/java/com/ejemplo/App.java
@@ -1,16 +1,11 @@
 package com.ejemplo;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-/**
- * Hello world!
- *
- */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer{
+public class App extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
