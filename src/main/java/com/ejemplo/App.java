75377b0dbead426440894680a3c3ff52179e7167
dsainz <dsainz@tragsa.es>
2025-03-05 12:06:42 +0100

    Convertida en war

--- src/main/java/com/ejemplo/App.java
+++ src/main/java/com/ejemplo/App.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer{
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
