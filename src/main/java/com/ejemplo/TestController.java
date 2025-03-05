b4b05ea9b4e5f7cf47b2334fa14c7bcf9147d7f1
dsainz <dsainz@tragsa.es>
2025-03-05 10:35:20 +0100

    Actualizado TestController

--- src/main/java/com/ejemplo/TestController.java
+++ src/main/java/com/ejemplo/TestController.java
@@ -1,4 +1,8 @@
 package com.ejemplo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api")
 public class TestController {
